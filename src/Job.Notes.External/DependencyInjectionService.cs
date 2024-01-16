f5fda7297e5cc488edb97ee9f6f7ad5cf0b7a78d
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-01-15 22:06:12 -0300

    Add config External

---
+++ src/Job.Notes.External/DependencyInjectionService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Job.Notes.External;
+
+public static class DependencyInjectionService
+{
+    public static IServiceCollection AddExternal(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        
+        return services;
+    }
+}
