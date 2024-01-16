c763af618da600b14502a0a161a10f9ca232fa6b
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-01-15 22:07:30 -0300

    Add config Api

---
+++ src/Job.Notes.Api/DependencyInjectionService.cs
@@ -0,0 +1,10 @@
+﻿namespace Job.Notes.Api;
+
+public static class DependencyInjectionService
+{
+    public static IServiceCollection AddWebApi(this IServiceCollection services)
+    {
+        // NOTE: register services for this project
+        return services;
+    }
+}
--- src/Job.Notes.Api/Program.cs
+++ src/Job.Notes.Api/Program.cs
@@ -1,22 +1,23 @@
-using Job.Notes.Application.Interfaces;
-using Job.Notes.Persistence.Database;
-using Microsoft.EntityFrameworkCore;
+using Job.Notes.Api;
+using Job.Notes.Application;
+using Job.Notes.Common;
+using Job.Notes.External;
+using Job.Notes.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services
-    .AddDbContext<DatabaseService>(opt => 
-        opt.UseSqlServer(builder.Configuration["SQLConnectionString"]));
+    .AddWebApi()
+    .AddCommon()
+    .AddApplication()
+    .AddExternal(builder.Configuration)
+    .AddPersistence(builder.Configuration);
 
-builder.Services.AddScoped<IDatabaseService, DatabaseService>();
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
-
+app.MapControllers();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,9 +25,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
 
 app.Run();
