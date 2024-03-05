f0c15cbecc30096c763e7d4fafc0b800c05d8cd1
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-03-05 20:36:40 -0300

    Add delete option to ArchiveSpace

--- src/Job.Notes.Application/Database/Space/Commands/ArchiveSpace/ArchiveSpace.cs
+++ src/Job.Notes.Application/Database/Space/Commands/ArchiveSpace/ArchiveSpace.cs
@@ -1,19 +1,36 @@
 ﻿using Job.Notes.Application.Database.Space.Repositories;
 using Job.Notes.Domain.Entities;
+using Job.Notes.Domain.Enums;
 
 namespace Job.Notes.Application.Database.Space.Commands.ArchiveSpace;
 
 public class ArchiveSpace: IArchiveSpace
 {
-    private readonly IWriteSpaceRepository _repository;
+    private readonly IWriteSpaceRepository _writeRepository;
+    private readonly IReadSpaceRepository _readRepository;
 
-    public ArchiveSpace(IWriteSpaceRepository repository)
+    public ArchiveSpace(IWriteSpaceRepository writeRepository, IReadSpaceRepository readRepository)
     {
-        _repository = repository;
+        _writeRepository = writeRepository;
+        _readRepository = readRepository;
     }
 
     public async Task<SpaceEntity> Execute(int id)
     {
-        return await _repository.PartialUpdate(id, new {Enabled = false});
+        var entity = await _readRepository.GetById(id);
+        var updateObject = new ArchiveSpaceModel
+        {
+            Enabled = false,
+            Deleted = false,
+            Status = SpaceStatusEnum.Paused
+        };
+
+        if (!entity.TasksLists.Any() && !entity.Notes.Any() && !entity.QuestionsLists.Any())
+            updateObject.Deleted = true;
+
+        if (entity.Status == SpaceStatusEnum.Finished)
+            updateObject.Status = entity.Status;
+            
+        return await _writeRepository.PartialUpdate(id, updateObject);
     }
 }
---
+++ src/Job.Notes.Application/Database/Space/Commands/ArchiveSpace/ArchiveSpaceModel.cs
@@ -0,0 +1,10 @@
+﻿using Job.Notes.Domain.Enums;
+
+namespace Job.Notes.Application.Database.Space.Commands.ArchiveSpace;
+
+public class ArchiveSpaceModel
+{
+    public bool Enabled { get; set; }
+    public bool Deleted { get; set; }
+    public SpaceStatusEnum Status { get; set; }
+}
