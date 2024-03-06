9bd3a07ad2fada57260b29b2cee66e68390173a0
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-03-06 12:07:14 -0300

    Add RegistrationSecurity

---
+++ src/Job.Notes.Api/Controllers/SecurityController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Job.Notes.Api.Controllers;
+
+[Route("api/security")]
+[ApiController]
+public class SecurityController: ControllerBase
+{
+}
---
+++ src/Job.Notes.Domain/Entities/RegistrationSecurityEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Job.Notes.Domain.Entities;
+
+public class RegistrationSecurityEntity
+{
+    public int Id { get; set; }
+    public string Mail { get; set; }
+    public int Code { get; set; }
+    public int UserId { get; set; }
+    public UserEntity User { get; set; }
+    public bool Enabled { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime ExpirationDate { get; set; }
+}
