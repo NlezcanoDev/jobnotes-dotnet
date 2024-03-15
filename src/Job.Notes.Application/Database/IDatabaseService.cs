b016f9d51c81fa7ce384ecb94305f1788737236c
Nahuel Lezcano <nlezcano.dev@gmail.com>
2024-03-15 12:41:53 -0300

    Add config AuthEntity, SecurityEntity and RegistrationSecurityEntity DbSet config application layer

--- src/Job.Notes.Application/Database/IDatabaseService.cs
+++ src/Job.Notes.Application/Database/IDatabaseService.cs
@@ -6,7 +6,10 @@ namespace Job.Notes.Application.Database;
 
 public interface IDatabaseService
 {
+    // Users
     DbSet<UserEntity> User { get; set; }
+    
+    // Application
     DbSet<ProjectEntity> Project { get; set; }
     DbSet<SpaceEntity> Space { get; set; }
     DbSet<QuestionListEntity> QuestionList { get; set; }
@@ -14,6 +17,11 @@ public interface IDatabaseService
     DbSet<TaskListEntity> TaskList { get; set; }
     DbSet<TaskEntity> Task { get; set; }
     DbSet<NoteEntity> Note { get; set; }
+    
+    // Security
+    DbSet<AuthEntity> Auth { get; set; }
+    DbSet<SecurityEntity> Security { get; set; }
+    DbSet<RegistrationSecurityEntity> RegistrationSecurity { get; set; }
 
     Task<bool> SaveAsync();
 }
